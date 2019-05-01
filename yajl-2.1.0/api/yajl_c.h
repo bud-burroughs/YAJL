f91359c3afd2432b5f441a931799b84b2fb5c0ea
bburroughs <wallace_burroughs@cable.comcast.com>
2019-05-01 09:26:27 -0400

    Add c headers

---
+++ yajl-2.1.0/api/yajl_c.h
@@ -0,0 +1,17 @@
+//
+//  yajl_c.h
+//  YAJL
+//
+//  Created by Burroughs, Wallace H on 5/1/19.
+//
+
+#ifndef yajl_c_h
+#define yajl_c_h
+
+#include "yajl_version.h"
+#include "yajl_tree.h"
+#include "yajl_common.h"
+#include "yajl_gen.h"
+#include "yajl_parse.h"
+
+#endif /* yajl_c_h */
